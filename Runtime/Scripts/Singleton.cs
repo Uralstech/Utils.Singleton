07130bc46d8910e37afb1deb76708303d19e4924
Udayshankar Ravikumar <uralstech@gmail.com>
2025-01-03 17:03:10 +0530

    Utils.Singleton v1.2.0: Updated variable accessibility and licensing details.

--- Runtime/Scripts/Singleton.cs
+++ Runtime/Scripts/Singleton.cs
@@ -1,3 +1,17 @@
+// Copyright 2024 URAV ADVANCED LEARNING SYSTEMS PRIVATE LIMITED
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using UnityEngine;
 
 namespace Uralstech.Utils.Singleton
@@ -25,8 +39,7 @@ namespace Uralstech.Utils.Singleton
         /// </summary>
         /// <remarks>
         /// If there is no set instance, this will try to find them. If more than one instance
-        /// is found, the extra instances are deleted. If none are found, a new instance is
-        /// created.
+        /// is found, the extra instances are deleted. If none are found, a new instance is created.
         /// </remarks>
         public static T Instance => CheckForInstanceAndDeleteExtras()
                                         ? s_instance
